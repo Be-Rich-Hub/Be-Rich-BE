0a5e1731e61ccd95420ec2c48052e49c7446279b
Ready-Bridge <hanjungyo5533@gmail.com>
2025-03-23 02:47:31 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/org/example/berichbe/BeRichBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.berichbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeRichBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeRichBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/berichbe/BeRichBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.berichbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeRichBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
