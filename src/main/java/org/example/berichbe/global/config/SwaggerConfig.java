f63a5f747f795639d11514000cb91847b9042a89
ryugeon <sksmsfbrjs@naver.com>
2025-07-13 18:41:09 +0900

    fix: [BR-22] swagger 설정 수정

--- src/main/java/org/example/berichbe/global/config/SwaggerConfig.java
+++ src/main/java/org/example/berichbe/global/config/SwaggerConfig.java
@@ -3,15 +3,9 @@ package org.example.berichbe.global.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @OpenAPIDefinition(
         info = @Info(title = "Be-Rich-BE API Document",
@@ -27,19 +21,4 @@ import java.util.List;
         bearerFormat = "JWT"
 )
 @Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .servers(List.of(new Server().url("http://localhost:8080"))); // 기본 서버
-    }
-
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("be-rich-api")
-                .pathsToMatch("/**")
-                .build();
-    }
-}
+public class SwaggerConfig {}
